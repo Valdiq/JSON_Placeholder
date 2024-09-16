b0d1ca13f9eaf1eb61f1491c100138922b8e8ed6
Valdiq <stasyshyn.vld@gmail.com>
2024-09-16 16:54:18 +0200

    init

---
+++ src/test/java/org/example/json_placeholder/JsonPlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.json_placeholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
